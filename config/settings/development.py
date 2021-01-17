fdce57f1756b5c2e1c05e9c80b8768525dcfc583
Mpoyi-Tshibuyi <mpoyitshibuyi63@gmail.com>
2021-01-17 06:57:29 +0200

    install debug toolbar

--- config/settings/development.py
+++ config/settings/development.py
@@ -9,16 +9,15 @@ from        .base               import *
 DEBUG = True
 
 
-# INSTALLED_APPS += [
-#     'debug_toolbar',
+INSTALLED_APPS += [
+    'debug_toolbar',
 
-# ]
+]
 
-# MIDDLEWARE += [
-#     'debug_toolbar.middleware.DebugToolbarMiddleware',
-# ]
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
 
-# DATABASE_ROUTERS = ['config.router.AuthRouter']
 
 
 
@@ -47,39 +46,39 @@ DATABASES = {
 
 
 # Tried with Sqlite3 database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 
 #DEBUG TOOLBAR SETTINGS
-# DEBUG_TOOLBAR_PANELS = [
-#     'debug_toolbar.panels.versions.VersionsPanel',
-#     'debug_toolbar.panels.timer.TimerPanel',
-#     'debug_toolbar.panels.settings.SettingsPanel',
-#     'debug_toolbar.panels.headers.HeadersPanel',
-#     'debug_toolbar.panels.request.RequestPanel',
-#     'debug_toolbar.panels.sql.SQLPanel',
-#     'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-#     'debug_toolbar.panels.templates.TemplatesPanel',
-#     'debug_toolbar.panels.cache.CachePanel',
-#     'debug_toolbar.panels.signals.SignalsPanel',
-#     'debug_toolbar.panels.logging.LoggingPanel',
-#     'debug_toolbar.panels.redirects.RedirectsPanel',
-#     'debug_toolbar.panels.profiling.ProfilingPanel',
-# ]
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
 
-# def show_toolbar(request):
-#     return True
+def show_toolbar(request):
+    return True
 
-# DEBUG_TOOLBAR_CONFIG = {
-#     'INTERCEPT_REDIRECTS'   : False,
-#     'SHOW_TOOLBAR_CALLBACK' : show_toolbar,
-# }
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS'   : False,
+    'SHOW_TOOLBAR_CALLBACK' : show_toolbar,
+}
 
 
 
